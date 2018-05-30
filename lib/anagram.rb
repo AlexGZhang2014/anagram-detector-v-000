7d96b1359c2c89a5dd68ef674d167060a06d3481
Alex Zhang <alexgzhang92@gmail.com>
2018-05-30 02:34:00 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,7 +7,8 @@ class Anagram
   
   def match(possible_anagrams)
     word_array = self.word.split('').sort
-    anagram_array = possible_anagrams.select {|word| word_array == word.split('').sort}
+    anagram_array = possible_anagrams.select do |word| word_array == word.split('').sort
+    end
   end
   
 end
